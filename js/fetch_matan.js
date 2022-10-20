35e61614de79753a1282753c8acd85a1dc684fb3
kakaporuqee <kakaporuqee@yandex.ru>
2022-10-20 15:00:01 +0000

    js fixed

--- js/fetch_matan.js
+++ js/fetch_matan.js
@@ -25,26 +25,39 @@ function parse_date(date) {
     return [day, months[Number(month)]]
 };
 
-(async () => {
-    const response = await fetch(
-      'https://api.github.com/repos/zimch/abstracts' +
+async function fetchNotes() {
+    console.log('fetch started')
+    let googleDocView = 'https://docs.google.com/viewer?url='
+    let repoAddr = 'https://api.github.com/repos/zimch/abstracts' +
       '/contents/math_anal/approved_absctracts'
+    const response = await fetch(
+      repoAddr
     );
     const data = await response.json();
-    let htmlString = '<ul class="index_list">';
+    let htmlString = '<ul class="index_list"> </ul>';
+
+    document.getElementsByTagName('body')[0].innerHTML += htmlString;
 
     for (let file of data) {
       let day = parse_date(file.name)[0];
       let month = parse_date(file.name)[1];
-      htmlString += `<li><a 
-          href="${file.path}/calculus.pdf" 
+
+      const newResponse = await fetch(
+        `${repoAddr}/${file.name}/calculus.pdf`
+      );
+      const newData = await newResponse.json();
+      console.log(newData);
+      let newHtmlStr = "";
+
+      newHtmlStr += `<li><a 
+          href="${googleDocView}${newData.download_url}"
           target="_blank"
           class="index_link"
         > 
         лекция ${day} ${month}
         </a></li>`;
+        document.getElementsByTagName('ul')[0].innerHTML += newHtmlStr
     }
+  }
 
-    htmlString += '</ul>';
-    document.getElementsByTagName('body')[0].innerHTML += htmlString;
-  })()
+fetchNotes()
